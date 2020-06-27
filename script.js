7cf06058ce1aef17db554f79b0ea20b65b6e64d0
Bikash <vikihunk@gmail.com>
2020-06-27 18:23:56 +0530

    Java Script demo

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("running demo")
+var userName = prompt("What is your Name?")
+var age = prompt("What is your age?")
+alert("Hello " + userName);
+alert("You are approx " + age*365.25 + " days old!");
